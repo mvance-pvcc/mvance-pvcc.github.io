a25319190f34c74e69e702bead9de95003f62211
mvance-pvcc <114623366+mvance-pvcc@users.noreply.github.com>
2023-01-11 10:59:56 -0500

    Week 1 Homework

---
+++ CSC201/problems.java
@@ -0,0 +1,23 @@
+// ******************************************************
+// Problems.java
+//
+// Provide lots of syntax errors for the user to correct.
+//
+// Authors: Lewis, Loftus, DuVall-Early, Vance
+// Last Modified Date: 1/11/23
+//*********************************************************
+public class problems
+{
+   // ***************************************************
+   // main method
+   // Starting point for all java programs
+   //**************************************************** Made line into comment
+   public static void main (String[] args) // made case corrections and added 'void'
+   {
+	System.out.println ("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+	System.out.println ("This program used to have lots of problems,");
+	System.out.println ("but if it prints this, you fixed them all."); //Added missing semicolon
+	System.out.println (" *** Hurray! ***");
+	System.out.println ("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+   }
+}
